f89d8b11c8bccdb553152bc8c7367c39f71f8d55
birdiewd <birdiewd@birdiewd.com>
2021-12-20 22:09:15 -0600

    remove unneeded auth file

--- pages/api/auth.tsx
+++
@@ -1,6 +0,0 @@
-import { supabaseClient } from '../../lib/client'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	supabaseClient.auth.api.setAuthCookie(req, res)
-}
